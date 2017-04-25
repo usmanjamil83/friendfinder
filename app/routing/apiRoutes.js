b65400348c4595ab510a5a97ae954fc32666d06b
usmanjamil83 <usmanjamil83@hotmail.com>
2017-04-25 17:42:16 -0400

    worked on routs + connection

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,23 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var data = require("../data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(data);
+  });
+};
